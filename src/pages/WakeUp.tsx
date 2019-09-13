ae5f24a9900ed8095551b52076024e93a96cd05b
Dirty Koder <dirtykoder@gmail.com>
2019-09-13 19:37:46 -0400

    Set up wake up late path

--- src/pages/WakeUp.tsx
+++ src/pages/WakeUp.tsx
@@ -12,6 +12,7 @@ import capriceTanktop from '../images/capriceTanktop.jpg'
 const PageContent: IPageComponent = (props) => {
   const hour = props.state.timeOfDay.getHours()
   const roommateIsAsleep = hour == 6 // Roommate wakes up at 7
+  const roommateIsAwake = hour == 7
 
   return (
     <Page>
@@ -35,6 +36,39 @@ const PageContent: IPageComponent = (props) => {
         </div>
       }
 
+      {roommateIsAwake &&
+        <div>
+          <p>
+            As you sit up, you see your roommate, Allison, climbing out of bed. She looks
+            quite grumpy. She's wearing a very low cut tanktop and a pair of dark blue panties.
+          </p>
+
+          <img src={capriceTanktop} style={globalStyles.image} />
+
+          <p>
+            "What the hell, {props.state.playerName}?!", she says, as her feet hit the floor.
+            "Do you have to snooze your alarm so many damn times?"
+          </p>
+
+          <p>
+            "Sorry," you say back, feeling a bit guilty.
+          </p>
+
+          <p>
+            You watch as she walks over to her dresser, digs around and pulls out
+            some clothes. Then she pulls her tanktop off and drops it to the floor,
+            giving you a great view of her bare back for a moment. A second later, the
+            view is gone, as she pulls on the shirt she pulled out, a white tanktop. Once
+            it's pulled down into place, she pulls a pair of jeans on.
+          </p>
+
+          <p>
+            As she starts brushing her hair, you snap out of your voyeuristic trance
+            and realize you have stuff to do.
+          </p>
+        </div>
+      }
+
       <Options>
         <p>What do you do?</p>
 
@@ -55,6 +89,15 @@ const PageContent: IPageComponent = (props) => {
         </Option>
 
 
+        {roommateIsAwake &&
+          <Option
+            onClick={() => {
+              // props.navigateToPage(SnoozeAlarm)
+            }}
+          >
+            <div>Talk to Allison</div>
+          </Option>
+        }
       </Options>
     </Page>
   )
